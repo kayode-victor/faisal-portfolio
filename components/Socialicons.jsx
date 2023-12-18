c7c39999c80eea262a81ad3cd379d85cdedb93c4
kayode-victor <kayode.o.victor@gmail.com>
2023-12-18 15:14:06 +0100

    Mew file working-8

--- components/Socialicons.jsx
+++ components/Socialicons.jsx
@@ -1,22 +1,10 @@
 import Link from "next/link";
-import {
-  RiInstagramFill,
-  RiFacebookFill,
-  RiLinkedinFill,
-  RiPinterestFill,
-} from "react-icons/ri";
+import { RiInstagramFill, RiLinkedinFill } from "react-icons/ri";
 const Socialicons = () => {
   return (
     <div className="flex gap-4 lg:gap-6 pr-4">
       <Link
-        href={""}
-        className="bg-[#aeaeae] p-2 rounded-full text-primary text-[20px] rounded-bl-none hover:bg-white hover:text-secondary hover:-translate-y-1 transition-all duration-500"
-      >
-        <RiFacebookFill />
-      </Link>
-
-      <Link
-        href={""}
+        href={"https://www.instagram.com/_dashmedia?igshid=OGQ5ZDc2ODk2ZA=="}
         className="bg-[#aeaeae] p-2 rounded-full text-primary text-[20px] rounded-bl-none hover:bg-white hover:text-secondary hover:-translate-y-1 transition-all duration-500"
       >
         <RiInstagramFill />
@@ -27,12 +15,6 @@ const Socialicons = () => {
         className="bg-[#aeaeae] p-2 rounded-full text-primary text-[20px] rounded-bl-none hover:bg-white hover:text-secondary hover:-translate-y-1 transition-all duration-500"
       >
         <RiLinkedinFill />
-      </Link>
-      <Link
-        href={""}
-        className="bg-[#aeaeae] p-2 rounded-full text-primary text-[20px] rounded-bl-none hover:bg-white hover:text-secondary hover:-translate-y-1 transition-all duration-500"
-      >
-        <RiPinterestFill />
       </Link>
     </div>
   );
