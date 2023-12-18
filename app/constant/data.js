c9725b80917c9240c9b80637312e2d3c7d60fc14
kayode-victor <kayode.o.victor@gmail.com>
2023-12-18 11:20:19 +0100

    Mew file working-5

--- app/constant/data.js
+++ app/constant/data.js
@@ -167,68 +167,141 @@ export const testimonialData = [
 
 export const portraitData = [
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893702/portrait-2_bzrvip.jpg",
     alt: "Image 1",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893715/portrait-20_omiznm.jpg",
     alt: "Image 2",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893716/portrait-18_slytqd.jpg",
+    alt: "Image 3",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893716/portrait-18_slytqd.jpg",
+    alt: "Image 4",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893719/portrait-6_l3bhwz.jpg",
+    alt: "Image 5",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893719/portrait-5_wbq58r.jpg",
+    alt: "Image 6",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893730/portrait-9_gpefbm.jpg",
+    alt: "Image 7",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893740/portrait-10_cdnusk.jpg",
+    alt: "Image 8",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893741/portrait-19_mgnjrf.jpg",
+    alt: "Image 9",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893733/portrait-3_dazslv.jpg",
+    alt: "Image 10",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893734/portrait-11_oid3ai.jpg",
+    alt: "Image 11",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893738/portrait-12_rrpa0x.jpg",
+    alt: "Image 12",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893747/portrait-16_hbvhde.jpg",
+    alt: "Image 13",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893746/portrait-15_vnenfi.jpg",
+    alt: "Image 14",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702893741/portrait-13_gvcvxg.jpg",
+    alt: "Image 15",
   },
 ];
 export const weddingData = [
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894315/wedding-1_yuyx18.jpg",
     alt: "Image 1",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894316/wedding-2_i2uobw.jpg",
     alt: "Image 2",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894318/wedding-3_qzvrqa.jpg",
+    alt: "Image 3",
+  },
+
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894349/wedding-6_hgdkzi.jpg",
+    alt: "Image 6",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894313/wedding-7_golrzk.jpg",
+    alt: "Image 7",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894361/wedding-8_on6ibr.jpg",
+    alt: "Image 8",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894348/wedding-9_i3xrzm.jpg",
+    alt: "Image 9",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894341/wedding-10_piblqi.jpg",
+    alt: "Image 10",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894348/wedding-11_jbn9uh.jpg",
+    alt: "Image 11",
   },
 ];
 export const commercialData = [
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894588/Commercial-1_uibcve.jpg",
     alt: "Image 1",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894592/Commercial-2_v36vae.jpg",
     alt: "Image 2",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894596/Commercial-3_racepn.jpg",
+    alt: "Image 3",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894592/Commercial-2_v36vae.jpg",
+    alt: "Image 4",
   },
   {
-    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702828677/cld-sample-5.jpg",
-    alt: "Image 2",
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894598/Commercial-5_jzcbxb.jpg",
+    alt: "Image 5",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894603/Commercial-6_lqzgcj.jpg",
+    alt: "Image 6",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894569/Commercial-10_h9f2f6.jpg",
+    alt: "Image 7",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894568/Commercial-8_i0wje0.jpg",
+    alt: "Image 8",
+  },
+  {
+    src: "https://res.cloudinary.com/dikzx4eyh/image/upload/v1702894568/Commercial-9_mh04ec.jpg",
+    alt: "Image 9",
   },
 ];
 export const cinematographyData = [
