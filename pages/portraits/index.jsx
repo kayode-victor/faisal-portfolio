f0ebff11c34de84856f752d18204d2ea82ec9385
kayode-victor <kayode.o.victor@gmail.com>
2023-12-20 02:47:19 +0100

    Almost done1

--- pages/portraits/index.jsx
+++ pages/portraits/index.jsx
@@ -2,18 +2,36 @@ import { useState } from "react";
 import { portraitData } from "@/app/constant/data";
 import Image from "next/image";
 import Link from "next/link";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoArrowForward, IoArrowBack } from "react-icons/io5";
 
 export default function Portraits() {
   const [model, setModel] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [tempSrc, setTempSrc] = useState("");
-  const getImg = (src) => {
-    setTempSrc(src);
+
+  const openModal = (index) => {
+    setCurrentIndex(index);
+    setTempSrc(portraitData[index].src);
     setModel(true);
   };
+
   const closeModal = () => {
     setModel(false);
   };
+
+  const nextImage = () => {
+    const nextIndex = (currentIndex + 1) % portraitData.length;
+    setCurrentIndex(nextIndex);
+    setTempSrc(portraitData[nextIndex].src);
+  };
+
+  const prevImage = () => {
+    const prevIndex =
+      (currentIndex - 1 + portraitData.length) % portraitData.length;
+    setCurrentIndex(prevIndex);
+    setTempSrc(portraitData[prevIndex].src);
+  };
+
   return (
     <section className="padding-container max-container  mt-16 mb-10  lg:mt-20 lg:mb-0">
       <div className="justify-center px-10 lg:px-16">
@@ -28,7 +46,7 @@ export default function Portraits() {
             <span> / Portrait Photos</span>
           </p>
         </div>
-        {/*Images*/}
+        {/* Images */}
         <div className={model ? "modal open" : "modal"} onClick={closeModal}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             <Image
@@ -38,28 +56,24 @@ export default function Portraits() {
               width={800}
               height={800}
             />
-            <IoClose onClick={() => setModel(false)} />
+            <IoClose onClick={closeModal} className="close-icon" />
+            <IoArrowBack onClick={prevImage} className="nav-icon prev" />
+            <IoArrowForward onClick={nextImage} className="nav-icon next" />
           </div>
         </div>
         <div className="gallery mb-4">
-          {portraitData.map((portrait, i) => {
-            return (
-              <div
-                className="pics"
-                key={i}
-                onClick={() => getImg(portrait.src)}
-              >
-                <Image
-                  className="rounded-lg"
-                  src={portrait.src}
-                  alt={portrait.alt}
-                  layout="responsive" // Important for responsive behavior
-                  width={1000} // Set an appropriate width (adjust as needed)
-                  height={600} // Set an appropriate height (adjust as needed)
-                />
-              </div>
-            );
-          })}
+          {portraitData.map((portrait, i) => (
+            <div className="pics" key={i} onClick={() => openModal(i)}>
+              <Image
+                className="rounded-lg"
+                src={portrait.src}
+                alt={portrait.alt}
+                layout="responsive"
+                width={1000}
+                height={600}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
