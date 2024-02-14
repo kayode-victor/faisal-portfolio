242c1c7112717f95795504413968b3108b4da2a4
kayode-victor <kayode.o.victor@gmail.com>
2024-02-14 03:26:29 +0100

    new meta data 3

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,20 +13,20 @@ export default function Layout({ children }) {
         <title>Faisal Gidado | Photography Portfolio</title>
         <meta
           name="description"
-          content="Explore stunning photography by Faisal Gidado, a talented photographer based in Quebec, Canada."
+          content="Discover the captivating world of photography by Faisal Gidado, an esteemed photographer based in Quebec, Canada. Explore a diverse collection of breathtaking images, including mesmerizing landscapes, soulful portraits, vibrant events, and compelling street photography."
         />
         <meta
           name="keywords"
-          content="Faisal Gidado, photography, photographer, Quebec, Canada, portfolio, images, photos, film maker, videography"
+          content="Faisal Gidado, dashmedia, photography, photographer, Quebec, Canada, portfolio, images, photos, landscape photography, portrait photography, event photography, street photography, artistic, creative, professional"
         />
-        <meta name="author" content="Your Name" />
+        <meta name="author" content="Faisal Gidado" />
         <meta
           property="og:title"
           content="Faisal Gidado | Photography Portfolio"
         />
         <meta
           property="og:description"
-          content="Explore stunning photography by Faisal Gidado, a talented photographer based in Quebec, Canada."
+          content="Discover the captivating world of photography by Faisal Gidado, an esteemed photographer based in Quebec, Canada. Explore a diverse collection of breathtaking images, including mesmerizing landscapes, soulful portraits, vibrant events, and compelling street photography."
         />
         <meta property="og:type" content="website" />
         <meta
@@ -39,7 +39,7 @@ export default function Layout({ children }) {
         />
         <meta property="og:image:alt" content="Photography Portfolio" />
         <meta property="og:locale" content="en_CA" />
-        <link rel="icon" href="./favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main>{children}</main>
